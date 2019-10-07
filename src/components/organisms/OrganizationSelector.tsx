1ab50efb870ebed953fe3ac67fa23a4897a08719
Jordy van Domselaar <jordy@deinternetjongens.nl>
2019-10-07 23:56:35 +0200

    master: If the user has previously selected an organization, remember it

--- src/components/organisms/OrganizationSelector.tsx
+++ src/components/organisms/OrganizationSelector.tsx
@@ -4,11 +4,18 @@ import {useSelector} from "react-redux";
 import {AppState} from "../../store";
 import {Organization} from "../../types/Organization";
 import {NavigationStackScreenComponent} from "react-navigation-stack"
+import {AsyncStorage} from "react-native";
 
 const OrganizationSelector: NavigationStackScreenComponent = ({navigation}) => {
     const organizations = useSelector<AppState, Organization[]>(state => state.organizations.organizations);
 
     const handleRedirect = (organizationId: string) => navigation.navigate("ProjectList", {organizationId});
+
+    AsyncStorage.getItem("organizationId").then(organizationId => {
+        if(organizationId) {
+            handleRedirect(organizationId);
+        }
+    });
 
     return (
         <OrganizationList organizations={organizations}
